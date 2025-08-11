6bc683dfc51ecf9b167cae278f2a974f4c428c48
AndreRojasMartinsson <89520206+AndreRojasMartinsson@users.noreply.github.com>
2025-08-11 14:07:52 +0200

    added actions export
    Signed-off-by: AndreRojasMartinsson <89520206+AndreRojasMartinsson@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./schemas";
 export * from "./context";
 export * from "./errors";
+export * from "./actions";
 
 export type { InferInput, InferOutput, Schema } from "./schema";
 export { is, parse } from "./schema";
