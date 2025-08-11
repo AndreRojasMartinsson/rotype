c1e25054db1dfb05bcac819fac4ec1b879ab4145
AndreRojasMartinsson <89520206+AndreRojasMartinsson@users.noreply.github.com>
2025-08-11 13:55:22 +0200

    cleaned up index
    Signed-off-by: AndreRojasMartinsson <89520206+AndreRojasMartinsson@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export * from "./schemas";
 export * from "./context";
 export * from "./errors";
-export * from "./schema";
+
+export type { InferInput, InferOutput, Schema } from "./schema";
+export { is, parse } from "./schema";
