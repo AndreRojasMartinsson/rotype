7dd57eb20202f7f6b2355f5d5cd97486891babcd
AndreRojasMartinsson <89520206+AndreRojasMartinsson@users.noreply.github.com>
2025-08-11 13:06:42 +0200

    create parse and is functions
    Signed-off-by: AndreRojasMartinsson <89520206+AndreRojasMartinsson@users.noreply.github.com>

--- src/schema.ts
+++ src/schema.ts
@@ -1,9 +1,24 @@
-import { Ctx, Result } from "./context";
+import { Ctx, makeCtx } from "./context";
+import { Issue } from "./errors";
+import { Result } from "./result";
 
 export interface Schema<I, O = I> {
 	kind: string;
-	_parse(data: unknown, ctx: Ctx): Result<O>;
+	_parse(data: unknown, ctx: Ctx): Result<O, Issue[]>;
 }
 
 export type InferInput<S> = S extends Schema<infer I, unknown> ? I : never;
 export type InferOutput<S> = S extends Schema<unknown, infer O> ? O : never;
+
+export function parse<S extends Schema<any, any>>(schema: S, data: unknown): Result<InferOutput<S>, Issue[]> {
+	const ctx = makeCtx();
+	const res = schema._parse(data, ctx);
+
+	return res;
+}
+
+export function is<S extends Schema<any, any>>(schema: S, data: unknown): data is InferOutput<S> {
+	const ctx = makeCtx();
+
+	return schema._parse(data, ctx).isOk();
+}
