bddb16e9618c48574919e0830ba941ed43c71f2a
AndreRojasMartinsson <89520206+AndreRojasMartinsson@users.noreply.github.com>
2025-08-11 12:59:43 +0200

    created result and option classes
    Signed-off-by: AndreRojasMartinsson <89520206+AndreRojasMartinsson@users.noreply.github.com>

---
+++ src/option.ts
@@ -0,0 +1,172 @@
+import type { Result as ResultType } from "./result";
+import { Result } from "./result";
+
+export class Option<T extends defined> {
+	protected constructor(protected readonly value: T | undefined) {}
+
+	public static none<T extends defined>(): Option<T> {
+		return new Option<T>(undefined);
+	}
+
+	public static some<T extends defined>(val: T): Option<T> {
+		return new Option(val);
+	}
+
+	public static wrap<T extends defined>(val: T | undefined): Option<T> {
+		return new Option(val);
+	}
+
+	public toString(): string {
+		return this.match(
+			val => `Option.some(${val})`,
+			() => "Option.none",
+		);
+	}
+
+	public isSome(): boolean {
+		return this.value !== undefined;
+	}
+
+	public isNone(): boolean {
+		return !this.isSome();
+	}
+
+	public contains(x: T): boolean {
+		return this.value === x;
+	}
+
+	public expect(msg: unknown): T | never {
+		if (this.isSome()) return this.value as T;
+		else throw msg;
+	}
+
+	public unwrap(): T | never {
+		return this.expect("called `Option.unwrap()` on a `None` value");
+	}
+
+	public unwrapOr(def: T): T {
+		return this.isSome() ? (this.value as T) : def;
+	}
+
+	public unwrapOrElse(gen: () => T): T {
+		return this.isSome() ? (this.value as T) : gen();
+	}
+
+	public map<U extends defined>(func: (item: T) => U): Option<U> {
+		return this.isSome() ? Option.some(func(this.value as T)) : Option.none();
+	}
+
+	public inspect(func: (item: T) => void): this {
+		if (this.isSome()) func(this.value as T);
+		return this;
+	}
+
+	public mapOr<U>(def: U, func: (item: T) => U): U {
+		return this.isSome() ? func(this.value as T) : def;
+	}
+
+	public mapOrElse<U>(def: () => U, func: (item: T) => U): U {
+		return this.isSome() ? func(this.value as T) : def();
+	}
+
+	public okOr<E extends defined>(err: E): ResultType<T, E> {
+		return this.isSome() ? Result.ok(this.value as T) : Result.err(err);
+	}
+
+	public okOrElse<E extends defined>(err: () => E): ResultType<T, E> {
+		return this.isSome() ? Result.ok(this.value as T) : Result.err(err());
+	}
+
+	public and<U extends defined>(other: Option<U>): Option<U> {
+		return this.isNone() ? Option.none() : other;
+	}
+
+	public andWith<U extends defined>(other: (val: T) => Option<U>): Option<U> {
+		return this.isSome() ? other(this.value as T) : Option.none();
+	}
+
+	public filter(func: (val: T) => boolean): Option<T> {
+		return this.isSome() ? (func(this.value as T) ? Option.some(this.value as T) : Option.none()) : Option.none();
+	}
+
+	public or(other: Option<T>): Option<T> {
+		return this.isSome() ? Option.some(this.value as T) : other;
+	}
+
+	public orElse(other: () => Option<T>): Option<T> {
+		return this.isSome() ? Option.some(this.value as T) : other();
+	}
+
+	public xor(other: Option<T>): Option<T> {
+		return this.isSome()
+			? other.isSome()
+				? Option.none()
+				: Option.some(this.value as T)
+			: other.isSome()
+				? Option.some(other.value as T)
+				: Option.none();
+	}
+
+	public zip<U extends defined>(other: Option<U>): Option<[T, U]> {
+		if (this.isSome() && other.isSome()) {
+			return Option.some([this.value as T, other.value as U]);
+		}
+		return Option.none();
+	}
+
+	public zipWith<U extends defined, R extends defined>(other: Option<U>, func: (self: T, other: U) => R): Option<R> {
+		if (this.isSome() && other.isSome()) {
+			return Option.some(func(this.value as T, other.value as U));
+		}
+		return Option.none();
+	}
+
+	public copied(): Option<T> {
+		return Option.wrap(this.value);
+	}
+
+	public cloned(this: Option<{ cloned: () => T }>): Option<T> {
+		return this.map(i => i.cloned());
+	}
+
+	public transpose<R extends defined, E extends defined>(this: Option<ResultType<R, E>>): ResultType<Option<R>, E> {
+		return this.isSome()
+			? this.value!.isOk()
+				? Result.ok(Option.some(this.value!.asPtr() as R))
+				: Result.err(this.value!.asPtr() as E)
+			: Result.ok(Option.none());
+	}
+
+	public flatten<I extends defined>(this: Option<Option<I>>): Option<I> {
+		return this.isSome() ? Option.wrap(this.value!.value) : Option.none();
+	}
+
+	/**
+	 * Executes one of two callbacks based on the type of the contained value.
+	 * Replacement for Rust's `match` expression.
+	 * @param ifSome Callback executed when this Option contains a Some value.
+	 * @param ifNone Callback executed when this Option contains a None value.
+	 */
+	public match<R>(ifSome: (val: T) => R, ifNone: () => R): R {
+		return this.isSome() ? ifSome(this.value as T) : ifNone();
+	}
+
+	/**
+	 * Returns the contained value directly. Only meant for special cases like serialisation.
+	 */
+	public asPtr(): T | undefined {
+		return this.value;
+	}
+}
+
+const optionMeta = Option as LuaMetatable<Option<never>>;
+optionMeta.__unm = option => option.map(item => -(item as number)) as Option<never>;
+optionMeta.__add = (option, other) => option.andWith(item => other.map(otherItem => (item + otherItem) as never));
+optionMeta.__sub = (option, other) => option.andWith(item => other.map(otherItem => (item - otherItem) as never));
+optionMeta.__mul = (option, other) => option.andWith(item => other.map(otherItem => (item * otherItem) as never));
+optionMeta.__div = (option, other) => option.andWith(item => other.map(otherItem => (item / otherItem) as never));
+optionMeta.__mod = (option, other) => option.andWith(item => other.map(otherItem => (item % otherItem) as never));
+optionMeta.__pow = (option, other) => option.andWith(item => other.map(otherItem => (item ^ otherItem) as never));
+
+optionMeta.__eq = (a, b) => a.asPtr() === b.asPtr();
+optionMeta.__len = option => option.map(item => (item as Array<never>).size()).unwrapOr(0);
---
+++ src/result.ts
@@ -0,0 +1,180 @@
+import type { Option as OptionType } from "./option";
+import { Option } from "./option";
+
+export interface UnitType {}
+
+const unitMeta: LuaMetatable<UnitType> = {};
+unitMeta.__eq = () => true;
+unitMeta.__tostring = () => "()";
+unitMeta.__index = () => error("Attempt to index Unit", 2);
+
+export function unit(): UnitType {
+	return setmetatable({}, unitMeta);
+}
+
+export class Result<T extends defined, E extends defined> {
+	private constructor(
+		protected readonly okValue: T | undefined,
+		protected readonly errValue: E | undefined,
+	) {}
+
+	public static ok<R extends defined, E extends defined>(val: R): Result<R, E> {
+		return new Result<R, E>(val, undefined);
+	}
+
+	public static err<R extends defined, E extends defined>(val: E): Result<R, E> {
+		return new Result<R, E>(undefined, val);
+	}
+
+	public static fromCallback<T extends defined>(c: () => T): Result<T, OptionType<defined>> {
+		const [success, result] = pcall(c);
+		return success ? Result.ok(result as T) : Result.err(Option.wrap(result as defined | undefined));
+	}
+
+	public static fromVoidCallback(c: () => void): Result<UnitType, OptionType<defined>> {
+		const [success, result] = pcall(c);
+		return success ? Result.ok(unit()) : Result.err(Option.wrap(result!));
+	}
+
+	public static fromPromise<T extends defined>(p: Promise<T>): Promise<Result<T, OptionType<defined>>> {
+		return p.then(
+			v => Result.ok(v),
+			e => Result.err(Option.wrap(e)),
+		);
+	}
+
+	public static fromVoidPromise(p: Promise<void>): Promise<Result<UnitType, OptionType<defined>>> {
+		return p.then(
+			() => Result.ok(unit()),
+			e => Result.err(Option.wrap(e)),
+		);
+	}
+
+	public toString(): string {
+		return this.match(
+			ok => `Result.ok(${ok})`,
+			err => `Result.err(${err})`,
+		);
+	}
+
+	public isOk(): boolean {
+		return this.okValue !== undefined;
+	}
+
+	public isErr(): boolean {
+		return this.errValue !== undefined;
+	}
+
+	public contains(x: T): boolean {
+		return this.okValue === x;
+	}
+
+	public containsErr(x: E): boolean {
+		return this.errValue === x;
+	}
+
+	public okOption(): OptionType<T> {
+		return Option.wrap(this.okValue);
+	}
+
+	public errOption(): OptionType<E> {
+		return Option.wrap(this.errValue);
+	}
+
+	public map<U extends defined>(func: (item: T) => U): Result<U, E> {
+		return this.isOk() ? Result.ok(func(this.okValue as T)) : Result.err(this.errValue as E);
+	}
+
+	public mapOr<U>(def: U, func: (item: T) => U): U {
+		return this.isOk() ? func(this.okValue as T) : def;
+	}
+
+	public mapOrElse<U>(def: (item: E) => U, func: (item: T) => U): U {
+		return this.isOk() ? func(this.okValue as T) : def(this.errValue as E);
+	}
+
+	public mapErr<F extends defined>(func: (item: E) => F): Result<T, F> {
+		return this.isErr() ? Result.err(func(this.errValue as E)) : Result.ok(this.okValue as T);
+	}
+
+	public inspect(func: (item: T) => void): this {
+		if (this.isOk()) func(this.okValue as T);
+		return this;
+	}
+
+	public inspectErr(func: (item: E) => void): this {
+		if (this.isErr()) func(this.errValue as E);
+		return this;
+	}
+
+	public and<U extends defined>(other: Result<U, E>): Result<U, E> {
+		return this.isErr() ? Result.err(this.errValue as E) : other;
+	}
+
+	public andWith<U extends defined>(func: (item: T) => Result<U, E>): Result<U, E> {
+		return this.isErr() ? Result.err(this.errValue as E) : func(this.okValue as T);
+	}
+
+	public or<F extends defined>(other: Result<T, F>): Result<T, F> {
+		return this.isOk() ? Result.ok(this.okValue as T) : other;
+	}
+
+	public orElse<F extends defined>(other: (item: E) => Result<T, F>): Result<T, F> {
+		return this.isOk() ? Result.ok(this.okValue as T) : other(this.errValue as E);
+	}
+
+	public expect(msg: unknown): T | never {
+		if (this.isOk()) return this.okValue as T;
+		else throw msg;
+	}
+
+	public unwrap(): T | never {
+		return this.expect(`called \`Result.unwrap()\` on an \`Err\` value: ${this.errValue}`);
+	}
+
+	public unwrapOr(def: T): T {
+		return this.isOk() ? (this.okValue as T) : def;
+	}
+
+	public unwrapOrElse(gen: (err: E) => T): T {
+		return this.isOk() ? (this.okValue as T) : gen(this.errValue as E);
+	}
+
+	public expectErr(msg: unknown): E | never {
+		if (this.isErr()) return this.errValue as E;
+		else throw msg;
+	}
+
+	public unwrapErr(): E | never {
+		return this.expectErr(`called \`Result.unwrapErr()\` on an \`Ok\` value: ${this.okValue}`);
+	}
+
+	public transpose<R extends defined, E extends defined>(this: Result<OptionType<R>, E>): OptionType<Result<R, E>> {
+		return this.isOk() ? this.okValue!.map(some => Result.ok(some)) : Option.some(Result.err(this.errValue as E));
+	}
+
+	public flatten<R extends defined, E extends defined>(this: Result<Result<R, E>, E>): Result<R, E> {
+		return this.isOk() ? new Result(this.okValue!.okValue, this.okValue!.errValue) : Result.err(this.errValue as E);
+	}
+
+	/**
+	 * Executes one of two callbacks based on the type of the contained value.
+	 * Replacement for Rust's `match` expression.
+	 * @param ifOk Callback executed when this Result contains an Ok value.
+	 * @param ifErr Callback executed when this Result contains an Err value.
+	 */
+	public match<R>(ifOk: (val: T) => R, ifErr: (err: E) => R): R {
+		return this.isOk() ? ifOk(this.okValue as T) : ifErr(this.errValue as E);
+	}
+
+	public asPtr(): T | E {
+		return (this.okValue as T) ?? (this.errValue as E);
+	}
+}
+
+const resultMeta = Result as LuaMetatable<Result<never, never>>;
+resultMeta.__eq = (a, b) =>
+	b.match(
+		ok => a.contains(ok),
+		err => a.containsErr(err),
+	);
